413b6b31eec342aa0d0ae33df59822516d22466a
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2022-12-05 01:50:46 +0100

    IGameMenuElement.h added

---
+++ src/IGameMenuElement.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+class Graphics;
+
+class IGameMenuElement {
+public:
+    virtual void setText(std::string text) = 0;
+    virtual void render(Graphics *graphics, int y, int x) = 0;
+    virtual bool isNotTextRender() = 0;
+    virtual void menuElemMethod(int var1) = 0;
+};
